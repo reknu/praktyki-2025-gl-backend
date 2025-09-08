bb75c39be784aea1d3b7e5dbda70b51beb9556d2
Krzysztof Nurek <krzysztof.nurek@globallogic.com>
2025-09-08 23:02:50 +0200

    KAN-879: Fix database status from integerfield to charfield (FREE/OCCUPIED) and push changes

--- core_parking/models/parking.py
+++ core_parking/models/parking.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 class Parking(models.Model):
+    STATUS_CHOICES = (
+        ('FREE', 'FREE'),
+        ('OCCUPIED', 'OCCUPIED'),
+    )
+
     spot_number = models.CharField(max_length=10)
     floor = models.IntegerField()
-    status = models.IntegerField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='FREE')
+
+    def __str__(self):
+        return self.spot_number
