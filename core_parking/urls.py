715d6dfe9875b8b6fa2547c7bc62f69e5be4c144
Rafal Walczynski <rafal.walczynski@globallogic.com>
2025-09-09 12:38:30 +0200

    Add user CRUD endpoint

--- core_parking/urls.py
+++ core_parking/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from .views import EmployeeList, UserList, VehicleList, ParkingList, ReservationList
-
+from django.urls import path, include
+from .views import EmployeeList, VehicleList, ParkingList, ReservationList
+from rest_framework.routers import DefaultRouter
+from .views.user import UserViewSet
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 urlpatterns = [
     path('employees/', EmployeeList.as_view()),
-    path('users/', UserList.as_view()),
+    path('', include(router.urls)),
     path('vehicles/', VehicleList.as_view()),
     path('parking/', ParkingList.as_view()),
     path('reservations/', ReservationList.as_view()),
