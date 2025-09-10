1a74316f579873f4ff290f34749d5e07a0cee5be
Maciej Sochaczewski <maciej.sochaczewski@globallogic.com>
2025-09-10 12:03:48 +0200

    vehicle crud

--- core_parking/urls.py
+++ core_parking/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
-from .views import EmployeeList, UserList, VehicleList, ParkingList, ReservationList
-
+from django.urls import path , include
+from .views import EmployeeList, UserList, ParkingList, ReservationList
+from rest_framework.routers import DefaultRouter
+from .views.vehicle import VehicleViewSet
+router = DefaultRouter()
+router.register(r'vehicle', VehicleViewSet, basename='vehicle')
 urlpatterns = [
     path('employees/', EmployeeList.as_view()),
     path('users/', UserList.as_view()),
-    path('vehicles/', VehicleList.as_view()),
+    path('', include(router.urls)),
     path('parking/', ParkingList.as_view()),
     path('reservations/', ReservationList.as_view()),
 ]
