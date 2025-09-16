912a7865e7cf81487c40fa34765cf0403b1d7564
Tomek <you@example.com>
2025-09-16 14:54:07 +0200

    added to urls

--- core_parking/urls.py
+++ core_parking/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.employee import EmployeeList
-from .views.user import UserList, UserViewSet 
-from .views.vehicle import VehicleList, VehicleViewSet
+# from .views.user import UserList, UserViewSet
+# from .views.vehicle import VehicleList, VehicleViewSet
 from .views.parking import ParkingList
-from .views.reservation import ReservationList
+from .views.reservation import ReservationList, UserReservationsList, LatestFiveReservations
 from .views.parking_detail import ParkingDetailWithAvailability
 router = DefaultRouter()
-router.register(r'vehicles', VehicleViewSet, basename='vehicle')
-router.register(r'users', UserViewSet, basename='user')
+# router.register(r'vehicles', VehicleViewSet, basename='vehicle')
+# router.register(r'users', UserViewSet, basename='user')
 urlpatterns = [
     path('employees/', EmployeeList.as_view(), name='employee-list'),
-    path('users/', UserList.as_view(), name='user-list'), 
-    path('vehicles/', VehicleList.as_view(), name='vehicle-list'),
+    # path('users/', UserList.as_view(), name='user-list'),
+    # path('vehicles/', VehicleList.as_view(), name='vehicle-list'),
     path('parking/', ParkingList.as_view(), name='parking-list'),
     path('parking/<int:pk>/', ParkingDetailWithAvailability.as_view(), name='parking-detail'),
-    path('reservations/', ReservationList.as_view(), name='reservation-list'),
+    path('reservations/', ReservationList.as_view(), name='reservation-list'), # co robi samo reservations, tworzy moze?
+    path('reservations/list', UserReservationsList.as_view(), name='reservation-list'),
+    path('reservations/list', LatestFiveReservations.as_view(), name='reservation-list'),
     path('', include(router.urls)),
 ]
